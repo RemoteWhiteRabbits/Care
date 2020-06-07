158c733d43b0ca23aa34537a0e117f19f1caf52c
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-06-07 17:36:28 +0200

    changed absolute path to relativeinside create services script

--- Service/create_services.ps1
+++ Service/create_services.ps1
@@ -3,5 +3,5 @@ Param([string[]]$servicesName)
 
 # for each service in servicesName verify the name of the project and call the script to create the service 
 foreach ($service in $servicesName){
-	& C:\personal\Care\Service\create_service.ps1 $service
+	& .\create_service.ps1 $service
 }
