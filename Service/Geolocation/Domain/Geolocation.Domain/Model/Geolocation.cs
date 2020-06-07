093e0e1e18aee2811954b3cee1dfc2d1dd409c31
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-06-07 17:54:20 +0200

    Added Geolocation model

---
+++ Service/Geolocation/Domain/Geolocation.Domain/Model/Geolocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geolocation.Domain.Model
+{
+    public class Geolocation
+    {
+        public Guid LocationId { get; set; }
+        public Guid ProfileId { get; set; }
+        public string Direction { get; set; }
+        public string Coordinates { get; set; }
+    }
+}
