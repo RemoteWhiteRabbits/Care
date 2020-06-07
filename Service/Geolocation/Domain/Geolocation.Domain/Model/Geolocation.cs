e3263bf40909a20adf2651c68181721702fdff81
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-06-07 19:04:55 +0200

    Added test for GEolocation model

--- Service/Geolocation/Domain/Geolocation.Domain/Model/Geolocation.cs
+++ Service/Geolocation/Domain/Geolocation.Domain/Model/Geolocation.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Geolocation.Domain.Model
 {
     public class Geolocation
     {
-        public Guid LocationId { get; set; }
-        public Guid ProfileId { get; set; }
-        public string Direction { get; set; }
-        public string Coordinates { get; set; }
+        public Guid LocationId { get; }
+        public Guid ProfileId { get; }
+        public string Direction { get; }
+        public string Coordinates { get; }
+
+        public Geolocation(Guid locationId, Guid profileId, string direction, string coordinates)
+        {
+            this.LocationId = locationId;
+            this.ProfileId = profileId;
+            this.Direction = direction;
+            this.Coordinates = coordinates;
+        }
     }
 }
