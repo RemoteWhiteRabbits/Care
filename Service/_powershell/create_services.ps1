d7cd71149f7aa0fbbc0f7a0104663888aca2374a
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-06-10 23:07:10 +0200

    moved and updated service creation scripts

--- Service/_powershell/create_services.ps1
+++ Service/_powershell/create_services.ps1
@@ -1,7 +1,9 @@
 # enter the names of the services proyects
 Param([string[]]$servicesName)
 
+cd ..
+
 # for each service in servicesName verify the name of the project and call the script to create the service 
 foreach ($service in $servicesName){
-	& .\create_service.ps1 $service
+	& .\_powershell\create_service.ps1 $service
 }
